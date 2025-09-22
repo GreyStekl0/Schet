d68188d4f799b91f347e2e0c519a81cfac42dd57
stekl0 <greystekl0@gmail.com>
2025-09-22 21:38:32 +0500

    Add JDK compatibility check

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,3 +29,11 @@ rootProject.name = "Schet"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
+
+check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
+    """
+    Now in Android requires JDK 17+ but it is currently using JDK ${JavaVersion.current()}.
+    Java Home: [${System.getProperty("java.home")}]
+    https://developer.android.com/build/jdks#jdk-config-in-studio
+    """.trimIndent()
+}
