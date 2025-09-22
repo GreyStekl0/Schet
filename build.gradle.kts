bb3a5a553514dcf2f0241ffd008e7ca0b1551fc6
stekl0 <greystekl0@gmail.com>
2025-09-22 21:24:01 +0500

    Add CauPain plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
+import com.deezer.caupain.model.StabilityLevelPolicy
+import com.deezer.caupain.plugin.DependenciesUpdateTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.caupain) apply true
+}
+
+tasks.withType<DependenciesUpdateTask> {
+    selectIf(StabilityLevelPolicy)
 }
